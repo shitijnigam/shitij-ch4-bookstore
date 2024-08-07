11f651c6d2b8b3e029d1671a4454908a80b1e837
Shitij Nigam <1710987+shitijnigam@users.noreply.github.com>
2024-08-08 02:17:33 +0800

    ch7

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third part
+    "crispy_forms",
+    "crispy_bootstrap5",
     # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
@@ -130,6 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+# STATICFILES_DIRS = ((os.path.join(BASE_DIR, "static")),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
